2c830b13abc961ef05ed5c45454ed21880e6cc8f
testalucida <martin.kendel@rotmax.de>
2020-08-13 09:51:32 +0200

    backup

--- various_tests/draggable_polygon.py
+++ various_tests/draggable_polygon.py
@@ -18,14 +18,39 @@ class DrawingPane( ScrollableView ):
         self._startX = 0
         self._startY = 0
 
+class Connection:
+    def __init__( self, item_id:int, conn_id:int, endpoint:int ):
+        self.item_id:int = item_id # item id
+        self.conn_id:int = conn_id # connection id
+        self.endpoint:int = endpoint #may be 1 or 2 and indicates which pair of coords() of the connection line
+        # belongs to the given item
+
+
+class Point:
+    def __init__( self, x:float, y:float ):
+        self.x = x
+        self.y = y
+
+
+class BoundingRect:
+    def __init__( self, item_id:int, rect_id:int, topLeft:Point, bottomRight:Point ):
+        self.item_id:int = item_id
+        self.rect_id:int = rect_id
+        self.topLeft:Point = topLeft
+        self.bottomRight:Point = bottomRight
+
 
 class Controller:
     def __init__( self, canvas:MyCanvas ):
         self.canvas = canvas
         self.canvas.bind( "<Button-1>", self.onCanvasLeftMouse )
-        self._selections:Dict[int, int] = {} # key: id of selected object; value: id of selection rectangle
-        self._connections:Dict[int,List[int]] = {} # key: object id; value: list of line id's related to key object
+        self._selections:Dict[int, BoundingRect] = {} # key: id of selected object; value: id and coords of selection rectangle
+        self._connections:Dict[int,List[Connection]] = {} # key: object id; value: list of collections referring to object id
         self._event_handled:bool = False
+        #self._affected_conns:Tuple[Tuple[int,int]] = () # stores connections affected by dragging an item in order to
+        # update them properly. The first Integer stores the id of the connection, the second the point to update.
+        self._startX = 0
+        self._startY = 0
 
     def onCanvasLeftMouse( self, evt:Event ):
         if not self._event_handled:
@@ -45,18 +70,20 @@ class Controller:
             self._unselect( id )
 
     def _select( self, id ):
-        rect:int = self._drawFocusRect( id )
-        self._selections[id] = rect
+        rect:int = self._createSelectionRect( id )
+
 
     def _unselect( self, id ):
-        sel_rect:int = self._selections[id]
-        self.canvas.delete( sel_rect )
+        sel_rect:BoundingRect = self._selections[id]
+        self.canvas.delete( sel_rect.rect_id )
         self._selections.pop( id )
 
     def isSelected( self, id:int ) -> bool:
-        return ( id in self._selections )
+        return ( id in self._selections.keys() )
 
     def onMouseEnter( self, id:int, evt:Event ):
+        if self.canvas.type( id ) == "line": return
+
         self.canvas["cursor"] = "hand2"
 
         # self.canvas["cursor"] = "fleur" # ok for moving object around
@@ -73,6 +100,7 @@ class Controller:
         self.canvas["cursor"] = "arrow"
 
     def onMouseMove( self, id: int, evt: Event ):
+        if id not in self._selections.keys(): return
         #self.canvas["cursor"] = "fleur"
         #self._setCursor( id, evt.x, evt.y )
         pass
@@ -86,13 +114,16 @@ class Controller:
             state: 1 == Shift
             state: 4 == Ctrl
         """
-        #print( "onLeftMouse: ", id, " -- event: ", evt, "; state=", evt.state )
+        if self.canvas.type( id ) == "line": return
+
         if not self.isSelected( id ):
             if evt.state & 1 == 1 or evt.state & 4 == 4: #Shift or Ctrl pressed
                 self._select( id )
             else:
                 self._clearSelections()
                 self._select( id )
+
+        # remember event position for dragging
         self._startX = evt.x
         self._startY = evt.y
         """
@@ -111,7 +142,7 @@ class Controller:
         self.canvas.move( id, dx, dy )
         #drag selection rectangle
         rect = self._selections[id]
-        self.canvas.move( rect, dx, dy )
+        self.canvas.move( rect.rect_id, dx, dy )
         #update connections
         self._updateConnections( id, dx, dy )
         # update startX and startY
@@ -119,29 +150,33 @@ class Controller:
         self._startY += dy
 
     def _updateConnections( self, id:int, dx, dy ) -> None:
-        # get center of item's surrounding rectangle
-        selrect_coords:Tuple[Tuple[float,float]] = self._getSelectionRectCoords( id )
-        conn_list:List[int] = self._connections[id]
+        conn_list:List[Connection] = self._connections[id]
         for conn in conn_list:
             # get coords of connection line
-            coords:List[float] = self.canvas.coords( conn )
-            xy:Tuple[float,float] = (coords[0], coords[1])
-            if self._isPointInRect( xy, selrect_coords ):
-                coords[0] += dx
-                coords[1] += dy
-                self.canvas.coords( conn, coords )
+            coords:List[float] = self.canvas.coords( conn.conn_id )
+            idx1, idx2 = (0, 1) if conn.endpoint == 1 else (2, 3)
+            xy:Tuple[float,float] = (coords[idx1], coords[idx2])
+            coords[idx1] += dx
+            coords[idx2] += dy
+            #print( "conn: update coords by dx=%d, dy=%d: " % (dx, dy), ": ", coords )
+            self.canvas.coords( conn.conn_id, coords )
 
     def _isPointInRect( self, point:Tuple[float,float], rect:Tuple[Tuple[float,float]] ) -> bool:
         px = point[0]
         py = point[1]
-        if px >= rect[0][0] and px <= rect[1][0] and \
-           py >= rect[0][1] and px <= rect[1][1]:
-           return True
+        if rect[0][0] <= px <= rect[1][0] and \
+           rect[0][1] <= py <= rect[1][1]:
+            return True
+
         return False
 
-    def _drawFocusRect( self, id:int ) -> int:
+    def _createSelectionRect( self, id:int ) -> int:
         vertices:Tuple[Tuple[float,float]] = self._getSelectionRectCoords( id )
         sel:int = self.canvas.create_rectangle( vertices, outline='yellow', width=2 )
+        topleft:Point = Point( vertices[0][0], vertices[0][1] )
+        bottomright: Point = Point( vertices[1][0], vertices[1][1] )
+        boundRect:BoundingRect = BoundingRect( id, sel, topleft, bottomright )
+        self._selections[id] = boundRect
         return sel
 
     def _getSelectionRectCenter( self, id:int ):
@@ -209,15 +244,20 @@ class Controller:
         x2, y2 =  self._getSelectionRectCenter( id2 )
         line = self.createLine( x1, y1, x2, y2 )
         # connect id1 and id2 with line
-        self._connect( id1, line )
-        self._connect( id2, line )
+        self._connect( id1, line, 1 )
+        self._connect( id2, line, 2 )
         return line
 
-    def _connect( self, id:int, line:int ) -> None:
-        if id in self._connections.keys(): self._connections[id].append( line )
-        else: self._connections[id] = [ line, ]
-        if line in self._connections.keys(): self._connections[line].append( id )
-        else: self._connections[line] = [ id, ]
+    def _connect( self, id:int, line:int, coord_number:int ) -> None:
+        conn:Connection = Connection( id, line, coord_number )
+        if id in self._connections.keys():
+            self._connections[id].append( conn )
+        else:
+            self._connections[id] = [ conn, ]
+        if line in self._connections.keys():
+            self._connections[line].append( conn )
+        else:
+            self._connections[line] = [ conn, ]
 
     def _bind( self, id:int ):
         self.canvas.tag_bind( id, "<Enter>", partial( self.onMouseEnter, id ) )
@@ -242,9 +282,13 @@ def test():
     xy = [(100, 20), (80, 70), (120, 70)]
     poly2 = cntrl.createPolygon( xy )
 
-    cntrl.createOval( 100, 100, 300, 150, fill='yellow' )
+    #cntrl.createOval( 100, 100, 300, 150, fill='yellow' )
 
     cntrl.connect( poly1, poly2 )
+
+    xy = [(80, 90), (160, 90), (160, 160), (80, 160)]
+    poly3 = cntrl.createPolygon( xy )
+    cntrl.connect( poly1, poly3 )
 
     root.mainloop()
 
